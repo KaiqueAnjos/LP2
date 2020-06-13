ddcc0e663b265841ffbb006eda1cd5fae473d720
KaiqueAnjos <61607397+KaiqueAnjos@users.noreply.github.com>
2020-06-12 23:46:36 -0300

    Add files via upload

---
+++ Atividade 7/PAtividade7/PAtividade7/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace PAtividade7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExer1 = new System.Windows.Forms.Button();
+            this.btnExer2 = new System.Windows.Forms.Button();
+            this.btnExer3 = new System.Windows.Forms.Button();
+            this.btnExer4 = new System.Windows.Forms.Button();
+            this.btnExer6 = new System.Windows.Forms.Button();
+            this.btnExer5 = new System.Windows.Forms.Button();
+            this.btnExer7 = new System.Windows.Forms.Button();
+            this.lboxNomes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExer1
+            // 
+            this.btnExer1.Location = new System.Drawing.Point(42, 60);
+            this.btnExer1.Name = "btnExer1";
+            this.btnExer1.Size = new System.Drawing.Size(109, 46);
+            this.btnExer1.TabIndex = 0;
+            this.btnExer1.Text = "Ler 20 Números e Inverter";
+            this.btnExer1.UseVisualStyleBackColor = true;
+            this.btnExer1.Click += new System.EventHandler(this.btnExer1_Click);
+            // 
+            // btnExer2
+            // 
+            this.btnExer2.Location = new System.Drawing.Point(190, 60);
+            this.btnExer2.Name = "btnExer2";
+            this.btnExer2.Size = new System.Drawing.Size(110, 46);
+            this.btnExer2.TabIndex = 1;
+            this.btnExer2.Text = "Ler 20 Números e Inverter(Reverse)";
+            this.btnExer2.UseVisualStyleBackColor = true;
+            this.btnExer2.Click += new System.EventHandler(this.btnExer2_Click);
+            // 
+            // btnExer3
+            // 
+            this.btnExer3.Location = new System.Drawing.Point(42, 151);
+            this.btnExer3.Name = "btnExer3";
+            this.btnExer3.Size = new System.Drawing.Size(109, 46);
+            this.btnExer3.TabIndex = 2;
+            this.btnExer3.Text = "Ler Quantidade e Preço Mercadoria";
+            this.btnExer3.UseVisualStyleBackColor = true;
+            this.btnExer3.Click += new System.EventHandler(this.btnExer3_Click);
+            // 
+            // btnExer4
+            // 
+            this.btnExer4.Location = new System.Drawing.Point(190, 151);
+            this.btnExer4.Name = "btnExer4";
+            this.btnExer4.Size = new System.Drawing.Size(110, 46);
+            this.btnExer4.TabIndex = 3;
+            this.btnExer4.Text = "Exercicio 4";
+            this.btnExer4.UseVisualStyleBackColor = true;
+            this.btnExer4.Click += new System.EventHandler(this.btnExer4_Click);
+            // 
+            // btnExer6
+            // 
+            this.btnExer6.Location = new System.Drawing.Point(190, 240);
+            this.btnExer6.Name = "btnExer6";
+            this.btnExer6.Size = new System.Drawing.Size(110, 46);
+            this.btnExer6.TabIndex = 4;
+            this.btnExer6.Text = "Exercicio 6";
+            this.btnExer6.UseVisualStyleBackColor = true;
+            this.btnExer6.Click += new System.EventHandler(this.btnExer6_Click);
+            // 
+            // btnExer5
+            // 
+            this.btnExer5.Location = new System.Drawing.Point(42, 240);
+            this.btnExer5.Name = "btnExer5";
+            this.btnExer5.Size = new System.Drawing.Size(109, 46);
+            this.btnExer5.TabIndex = 5;
+            this.btnExer5.Text = "Exercicio 5";
+            this.btnExer5.UseVisualStyleBackColor = true;
+            this.btnExer5.Click += new System.EventHandler(this.btnExer5_Click);
+            // 
+            // btnExer7
+            // 
+            this.btnExer7.Location = new System.Drawing.Point(399, 162);
+            this.btnExer7.Name = "btnExer7";
+            this.btnExer7.Size = new System.Drawing.Size(99, 64);
+            this.btnExer7.TabIndex = 6;
+            this.btnExer7.Text = "Executar";
+            this.btnExer7.UseVisualStyleBackColor = true;
+            this.btnExer7.Click += new System.EventHandler(this.btnExer7_Click);
+            // 
+            // lboxNomes
+            // 
+            this.lboxNomes.FormattingEnabled = true;
+            this.lboxNomes.Location = new System.Drawing.Point(504, 103);
+            this.lboxNomes.Name = "lboxNomes";
+            this.lboxNomes.Size = new System.Drawing.Size(307, 277);
+            this.lboxNomes.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 450);
+            this.Controls.Add(this.lboxNomes);
+            this.Controls.Add(this.btnExer7);
+            this.Controls.Add(this.btnExer5);
+            this.Controls.Add(this.btnExer6);
+            this.Controls.Add(this.btnExer4);
+            this.Controls.Add(this.btnExer3);
+            this.Controls.Add(this.btnExer2);
+            this.Controls.Add(this.btnExer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExer1;
+        private System.Windows.Forms.Button btnExer2;
+        private System.Windows.Forms.Button btnExer3;
+        private System.Windows.Forms.Button btnExer4;
+        private System.Windows.Forms.Button btnExer6;
+        private System.Windows.Forms.Button btnExer5;
+        private System.Windows.Forms.Button btnExer7;
+        private System.Windows.Forms.ListBox lboxNomes;
+    }
+}
+
---
+++ Atividade 7/PAtividade7/PAtividade7/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Collections;
+
+namespace PAtividade7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExer1_Click(object sender, EventArgs e)
+        {
+            int[] Vetor = new int[20];
+            string valor = "";
+            string auxiliar = "";
+
+            for (var x = 0; x < 20; x++)
+            {
+                valor = Interaction.InputBox("Digite um número:" + (x + 1), "Entrada de dados");
+                if (valor == "")
+                {
+                    break;
+                }
+                if (int.TryParse(valor, out Vetor[x]))
+                {
+                    //auxiliar = auxiliar + "\n" + Vetor[x].ToString();
+                    auxiliar = Vetor[x].ToString() + "\n" + auxiliar;
+                }
+                else
+                {
+                    MessageBox.Show("Número Inválido!");
+                    x--;
+                }
+            }
+            MessageBox.Show(auxiliar);
+        }
+
+        private void btnExer2_Click(object sender, EventArgs e)
+        {
+            int[] Vetor = new int[20];
+            string valor = "";
+            string auxiliar = "";
+
+            for (var x = 0; x < 20; x++)
+            {
+                valor = Interaction.InputBox("Digite um número:" + (x + 1), "Entrada de dados");
+                if (valor == "")
+                {
+                    break;
+                }
+                if (int.TryParse(valor, out Vetor[x]))
+                {
+                    MessageBox.Show("Número Inválido!");
+                    x--;
+                }
+
+            }
+            Array.Reverse(Vetor);
+            for (var x = 0; x < 20; x++)
+            {
+                auxiliar = auxiliar + "\n" + Vetor[x];
+            }
+            MessageBox.Show(auxiliar);
+        }
+
+        private void btnExer3_Click(object sender, EventArgs e)
+        {
+            int[] Quantidade = new int[10];
+            double[] Preco = new double[10];
+            string valor;
+            double faturamento = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                valor = Interaction.InputBox("Digite a quantidade vendida do produto:" + (i + 1), 
+                    "Entrada de Quantidade");
+                if (int.TryParse(valor, out Quantidade[i]))
+                {
+                    while (true)
+                    {
+                        valor = Interaction.InputBox("Digite o valor da mercadoria:" + (i + 1),
+                            "Entrada de Preço");
+                        if (double.TryParse(valor, out Preco[i]))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Preço Inválido para Mercadoria" + (i + 1));
+                        }
+                        
+                    }
+                    faturamento += Quantidade[i] * Preco[i];
+                }
+                else
+                {
+                    MessageBox.Show("Quantidade Inválida da Mercadoria" + (i + 1));
+                    i--;
+                }
+            }
+            MessageBox.Show("Faturamento total = R$" + faturamento.ToString("N2"));
+
+        }
+
+        private void btnExer4_Click(object sender, EventArgs e)
+        {
+            string[] Alunos = {"Viviane", "André", "Hélio", "Denise", "Junior",
+                                "Leonardo", "Jose", "Nelma", "Tobby"};
+            Int32 I, Total = 0;
+            Int32 N = Alunos.Length;
+            for (I = 0; I < N - 1; I++)
+            {
+                Total += Alunos[I].Length;
+            }
+
+            MessageBox.Show("O resultado é: " + Total);
+        }
+
+        private void btnExer5_Click(object sender, EventArgs e)
+        {
+            string[] Alunos = {"Ana", "André", "Débora", "Fátima", "João", "Janete", "Otávio", "Marcelo",
+                "Pedro", "Thais" };
+            string auxiliar = "";
+            ArrayList AlunosList = new ArrayList();
+            AlunosList.AddRange(Alunos);
+            AlunosList.Remove("Otávio");
+            foreach (String nome in AlunosList)
+            {
+                auxiliar += nome + "\n";
+            }
+            MessageBox.Show(auxiliar);
+        }
+
+        private void btnExer6_Click(object sender, EventArgs e)
+        {
+            double[,] Notas = new double[20, 3];
+            double media = 0;
+            string nota;
+            string mediaAluno = "";
+
+            for (int x = 0; x < 20 ; x++)
+            {
+                for (int y = 0; y < 3 ; y++)
+                {
+                    nota = Interaction.InputBox("Digite a nota " + (y + 1) + " do Aluno " + (x +1),
+                        "Entrada de dados");
+                    if (double.TryParse(nota, out Notas[x,y]))
+                    {
+                        if (Notas[x,y] < 0)
+                        {
+                            MessageBox.Show("O valor da nota não pode ser menor que 0");
+                            y--;
+                        }
+                        else if (Notas[x,y] > 10)
+                        {
+                            MessageBox.Show("O valor da nota não pode ser superior a 10");
+                            y--;
+                        }
+                        else
+                        {
+                            double.TryParse(nota, out Notas[x, y]);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Digite apenas números");
+                        y--;
+                    }
+                }
+            }
+
+            for (int x = 0; x < 20; x++)
+            {
+                media = 0;
+                for (int y = 0; y < 3; y++)
+                {
+                    media = media + Notas[x, y];
+                }
+                media = media / 3;
+                mediaAluno = mediaAluno + "Aluno " + (x + 1) + ":" + " Média: " + media + "\n";
+            }
+
+            MessageBox.Show(mediaAluno);
+        }
+
+        private void btnExer7_Click(object sender, EventArgs e)
+        {
+            string[] Nome = new string[6];
+            int[] NCaracter = new int[6];
+            string nome;
+
+            for (int x = 0; x < 6; x++)
+            {
+                nome = Interaction.InputBox("Digite o nome completo da pessoa " + (x + 1),
+                    " Entrada de dados ");
+                if (nome =="")
+                {
+                    MessageBox.Show("Campo está vazio");
+                    x--;
+                }
+                else
+                {
+                    Nome[x] = nome;
+                    NCaracter[x] = nome.Replace(" ", "").Length; 
+                }
+            }
+
+            for (int x = 0; x < 6; x++)
+            {
+                lboxNomes.Items.Add("O nome: " + Nome[x] + " tem " + NCaracter[x] + " Caracteres");
+            }
+        }
+    }
+}
